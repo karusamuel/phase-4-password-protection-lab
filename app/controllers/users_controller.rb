ca2b4ae82537487daa16af58b902fbe80acf3d23
Daniel-Karanja <daniel.karanja@moringachool.com>
2022-09-25 17:48:17 +0300

    complete Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   
     private
   
+    
     def authorize
       return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     end
